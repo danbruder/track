aa5afbb095167dee613a2edef450e15b70fa361a
Dan Bruder <ddbruder@gmail.com>
2018-08-07 11:33:57 -0400

    Adding selectize

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,19 @@
 import 'jquery-ujs';
 import Turbolinks from 'turbolinks';
 import feather from 'feather-icons';
+import selectize from 'selectize';
+import 'selectize/dist/css/selectize.css';
+import 'selectize/dist/css/selectize.default.css';
+
+// Start Turbolinks
 Turbolinks.start();
 
+// Document ready callback
 $(document).on('turbolinks:load', function() {
+  // Replace feather icons
   feather.replace();
+
+  // Initialize select options
+  var options = {};
+  $('select').selectize(options);
 });
